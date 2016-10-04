d03cae9d14b6bf639cb34f6b3f3bf557401c69dd
Efthemi <ehatz01@gmail.com>
2016-10-04 15:16:09 -0400

    added .env functionality with dotenv package, modified slack api to work through server using env variable for webhook

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const express = require('express');
 const app = express();
-const db = require('dotenv').config();
+var request = require('request');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
+
+app.connect({
+	host: process.env.SLACK_WEBHOOK,
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -19,6 +24,21 @@ app.get('/', (req, res) => {
 
 app.get("/slack", (req, res) => {
 	res.sendFile(path.join(__dirname, './slack.html'));
+})
+
+app.post('/slack', (req, res) => {
+	request.post({
+		headers: {'content-type' : 'application/x-www-form-urlencoded'},
+		url: process.env.SLACK_WEBHOOK,
+		body: JSON.stringify({
+			'channel': req.body.channel, 
+			'username': 'webhookbot', 
+			'text': req.body.message, 
+			'icon_emoji': ':ghost:'
+		})
+	}, function(error, response, body){
+		console.log(body);
+	});
 })
 
 app.listen(PORT, () => {
