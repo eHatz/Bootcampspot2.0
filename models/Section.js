7054f3f5b52bfe0042ad59efa3893ee1044316c6
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-10-09 14:46:56 -0400

    Adding section model

---
+++ models/Section.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+	const Section = sequelize.define('Section', {
+		Title: DataTypes.STRING,
+		Location: DataTypes.STRING,
+		StartDate: DataTypes.DATEONLY,
+		EndDate: DataTypes.DATEONLY,
+	}, {
+	classMethods: {
+		associate: function(models) {
+			// associations can be defined here
+
+			Section.hasMany(Session, {as: "SessionOfSection"});
+			// Section.belongsToMany(User, {through: "UserSection"}
+
+
+		}
+	}
+});
+return Section;
+};
