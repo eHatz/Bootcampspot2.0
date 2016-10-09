04df9b36e764ba112e0445245fa5af82f2b42cd0
Efthemi <ehatz01@gmail.com>
2016-10-09 10:58:13 -0400

    added github login, currently debugging

--- Components/Navbar/Navbar.jsx
+++ Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ var Navbar = React.createClass ({
 
 		return (
 			<div>
-				<div id="navBar">
+				<div id="navBarDiv">
 					<ul>
 						<li><NavBarLink name="Attendance" img="/assets/images/attendance_nav.png" navLink="#/attendance"/></li>
 						<li><NavBarLink name="Homework" img="assets/images/homework_nav.png" navLink="#/homework"/></li> 
@@ -26,31 +26,3 @@ var Navbar = React.createClass ({
 
 export default Navbar;
 
-// <div className = "row">
-// 					<div className = "col-md-3">
-// 						<div className = {styles.Navbar_title}>
-// 							<h4 className = {styles.Navbar_h4}>RUTGERS UNIVERSITY</h4>
-// 							<h1 className = {styles.Navbar_h1red}>BOOTCAMP</h1>
-// 							<h1 className = {styles.Navbar_h1white}>SPOT</h1>
-// 							<h1 className = {styles.Navbar_h1red}>2.0</h1>
-// 						</div>
-// 					</div>
-// 					<div className = "col-md-9">
-// 						<div className = {styles.Navbar_redPanel}>
-// 							<p>Hi Jennine</p>
-// 							<p>Logout</p>
-// 						</div>
-// 					</div>
-// 				</div>
-// 				<div className = "row">
-// 					<div className = "col-md-3">
-// 						<div className = {styles.Navbar_blackpanel}>
-// 							<p>Attendence</p>
-// 							<p>Homework</p>
-// 							<p>Syllabus</p>
-// 							<p>Projects</p>
-// 							<p>Feedback</p>
-// 							<p>Career</p>
-// 						</div>
-// 					</div>
-// 				</div>
--- public/bundle.js
+++ public/bundle.js
@@ -44748,7 +44748,7 @@
 				null,
 				_react2.default.createElement(
 					"div",
-					{ id: "navBar" },
+					{ id: "navBarDiv" },
 					_react2.default.createElement(
 						"ul",
 						null,
@@ -44789,35 +44789,6 @@
 	});
 
 	exports.default = Navbar;
-
-	// <div className = "row">
-	// 					<div className = "col-md-3">
-	// 						<div className = {styles.Navbar_title}>
-	// 							<h4 className = {styles.Navbar_h4}>RUTGERS UNIVERSITY</h4>
-	// 							<h1 className = {styles.Navbar_h1red}>BOOTCAMP</h1>
-	// 							<h1 className = {styles.Navbar_h1white}>SPOT</h1>
-	// 							<h1 className = {styles.Navbar_h1red}>2.0</h1>
-	// 						</div>
-	// 					</div>
-	// 					<div className = "col-md-9">
-	// 						<div className = {styles.Navbar_redPanel}>
-	// 							<p>Hi Jennine</p>
-	// 							<p>Logout</p>
-	// 						</div>
-	// 					</div>
-	// 				</div>
-	// 				<div className = "row">
-	// 					<div className = "col-md-3">
-	// 						<div className = {styles.Navbar_blackpanel}>
-	// 							<p>Attendence</p>
-	// 							<p>Homework</p>
-	// 							<p>Syllabus</p>
-	// 							<p>Projects</p>
-	// 							<p>Feedback</p>
-	// 							<p>Career</p>
-	// 						</div>
-	// 					</div>
-	// 				</div>
 
 /***/ },
 /* 477 */
@@ -45168,7 +45139,7 @@
 				null,
 				_react2.default.createElement(
 					"div",
-					{ id: "navBar" },
+					{ id: "navBarDiv" },
 					_react2.default.createElement(
 						"ul",
 						null,
@@ -45209,35 +45180,6 @@
 	});
 
 	exports.default = Navbar;
-
-	// <div className = "row">
-	// 					<div className = "col-md-3">
-	// 						<div className = {styles.Navbar_title}>
-	// 							<h4 className = {styles.Navbar_h4}>RUTGERS UNIVERSITY</h4>
-	// 							<h1 className = {styles.Navbar_h1red}>BOOTCAMP</h1>
-	// 							<h1 className = {styles.Navbar_h1white}>SPOT</h1>
-	// 							<h1 className = {styles.Navbar_h1red}>2.0</h1>
-	// 						</div>
-	// 					</div>
-	// 					<div className = "col-md-9">
-	// 						<div className = {styles.Navbar_redPanel}>
-	// 							<p>Hi Jennine</p>
-	// 							<p>Logout</p>
-	// 						</div>
-	// 					</div>
-	// 				</div>
-	// 				<div className = "row">
-	// 					<div className = "col-md-3">
-	// 						<div className = {styles.Navbar_blackpanel}>
-	// 							<p>Attendence</p>
-	// 							<p>Homework</p>
-	// 							<p>Syllabus</p>
-	// 							<p>Projects</p>
-	// 							<p>Feedback</p>
-	// 							<p>Career</p>
-	// 						</div>
-	// 					</div>
-	// 				</div>
 
 /***/ },
 /* 491 */
