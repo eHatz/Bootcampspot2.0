422e2b889d9b475097ccac7bca07ce0c423d59cd
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2016-10-10 18:09:59 -0400

    ipad fix

--- auth.js
+++ auth.js
@@ -38,7 +38,7 @@ module.exports = {
 
 function pretendRequest(email, pass, cb) {
   setTimeout(() => {
-    if (email === 'ehatz01@gmail.com' && pass === 'password1') {
+    if (email === 'jennine@optonline.net' && pass === 'password1') {
       cb({
         authenticated: true,
         token: Math.random().toString(36).substring(7)
