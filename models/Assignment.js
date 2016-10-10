b23464cb0635929523ba87944796b2f7a944fc64
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-10-10 12:28:44 -0400

    switching to master

--- models/Assignment.js
+++ models/Assignment.js
@@ -11,7 +11,7 @@ module.exports = function(sequelize, DataTypes) {
 			// associations can be defined here
 
 			Assignment.belongsTo(Section)
-			Assignment.belongsToMany(Users, {through: "Submission"});
+			Assignment.belongsToMany(Users, {through: "UserAssignment"});
 			
 		}
 	}
