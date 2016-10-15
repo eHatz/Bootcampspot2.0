34b460577ffca2e0a11f43bd8103947e9567f73e
Nathaniel LaPier <Nat@Nathaniels-MacBook-Air.local>
2016-10-15 12:23:18 -0400

    adding our team as admins to users in seeder file

--- migrations/20161010211103-unnamed-migration.js
+++ migrations/20161010211103-unnamed-migration.js
@@ -4,6 +4,30 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     queryInterface.bulkInsert('Users', [
       {
+        FirstName: "Nat",
+        LastName: "LaPier",
+        Email: "nathaniel.lapier@gmail.com",
+        Role: "Admin"
+      },
+      {
+        FirstName: "Jennine",
+        LastName: "Punzone",
+        Email: "jennine@optonline.net",
+        Role: "Admin"
+      },
+      {
+        FirstName: "Tim",
+        LastName: "Hatzis",
+        Email: "ehatz01@gmail.com",
+        Role: "Admin"
+      },
+      {
+        FirstName: "Anda",
+        LastName: "Szilagyi",
+        Email: "ciganaromungra@gmail.com",
+        Role: "Admin"
+      },
+      {
         FirstName: "J",
         LastName: "Dogg",
         Email: "jdogg@nofilter.com",
