eeb4a3d8a7d4ade738935205fff44c980f001f9f
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-10-16 18:22:33 -0400

    added a 'seeder' file that can be run through node

---
+++ seeders/seedSection.js
@@ -0,0 +1,17 @@
+module.exports = [
+      {
+        Title: "The Illest Section",
+        Location: "CCC",
+        Slack: "bootcampspot2.slack.com",
+        StartDate: "2016-10-16 12:45:34",
+        EndDate: "2017-3-10 12:45:34"
+      },
+      {
+        Title: "The 'special' Section",
+        Location: "Short bus",
+        Slack: "bootcampspot2.slack.com",
+        StartDate: "2016-9-16 12:45:34",
+        EndDate: "2017-2-10 12:45:34"
+      }
+    ]
+
