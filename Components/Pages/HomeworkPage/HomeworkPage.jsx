5727538b435a52278a22b3ce5777224fbe2c7f4a
Efthemi <ehatz01@gmail.com>
2016-10-19 11:42:19 -0400

    fixed homework page errors

--- Components/Pages/HomeworkPage/HomeworkPage.jsx
+++ Components/Pages/HomeworkPage/HomeworkPage.jsx
@@ -69,7 +69,7 @@ class HomeworkPage extends Component {
 	render() {
 		const { UserInfo, UserSection } = this.props;
 		return (
-			{/*
+
 			<div className="homeworkBackground">
 				{UserInfo.UserInfo.Role === 'Admin' ? (
 					<CreateAssignment
@@ -94,13 +94,6 @@ class HomeworkPage extends Component {
 				<div className='wholeTable'>
 					<TableRow 
 						columnCount ={[
-
-							Instructions: 
-							Due:
-							SubmitStatus:
-							Type:
-							Resources:
-
 							{type: 'Header', value: 'TITLE'},
 							{type: 'Header', value: 'DUE DATE'},
 							{type: 'Header', value: 'TYPE'},
@@ -125,7 +118,6 @@ class HomeworkPage extends Component {
 					)}
 				</div>
 			</div>
-		*/}
 		);
 	}
 }
--- routes.jsx
+++ routes.jsx
@@ -65,6 +65,7 @@ export default (
 			<Route path="/attendance" component={AttendancePage}/>
 			<Route path="/career" component={CareerPage}/>
 			<Route path="/homework" component={HomeworkPage}/>
+			<Route path="/homework/:id" component={HomeworkPage}/>
 			<Route path="/syllabus" component={SyllabusPage}/>
 			<Route path="/feedback" component={FeedbackPage}/>
 			<Route path="/projects" component={ProjectsPage}/>
