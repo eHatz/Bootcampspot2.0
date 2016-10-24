56388d314ab31bd5c136db9ce4c8089c00f54be6
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-10-24 15:57:39 -0400

    adding heroku seeders

---
+++ seeders/herokuSeedUser.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    FirstName: "Nat",
+    LastName: "LaPier",
+    Email: "nathaniel.lapier@gmail.com",
+    Role: "Admin"
+  },
+  {
+    FirstName: "Jennine",
+    LastName: "Punzone",
+    Email: "jennine@optonline.net",
+    Role: "Admin"
+  },
+  {
+    FirstName: "Tim",
+    LastName: "Hatzis",
+    Email: "ehatz01@gmail.com",
+    Role: "Admin"
+  },
+  {
+    FirstName: "Anda",
+    LastName: "Szilagyi",
+    Email: "ciganaromungra@gmail.com",
+    Role: "Admin"
+  }
+]
