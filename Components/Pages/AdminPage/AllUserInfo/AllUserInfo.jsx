0816a6eee0496f603beacedfa1692ee29cce1c05
Efthemi <ehatz01@gmail.com>
2016-10-26 02:32:44 -0400

    added update user form, and pills for admin view student

--- Components/Pages/AdminPage/AllUserInfo/AllUserInfo.jsx
+++ Components/Pages/AdminPage/AllUserInfo/AllUserInfo.jsx
@@ -12,13 +12,20 @@ class AllUserInfo extends Component {
 			firstName:"",
 			lastName:"",
 			role:"",
-			sectionTitle: ""
+			sectionTitle: "",
+			sectionList: [],
+			AttendanceTab: 'active',
+			CareerTab: 'inactive',
+			HomeworkTab: 'inactive',
 		};
 		this.getSections = this.getSections.bind(this);
+		this.HomeworkTabClick = this.HomeworkTabClick.bind(this);
+		this.CareerTabClick = this.CareerTabClick.bind(this);
+		this.AttendanceTabClick = this.AttendanceTabClick.bind(this);
 	}
 
 	componentWillMount() {
-		
+		this.getSections();
 	}
 
 	getSections() {
@@ -30,16 +37,56 @@ class AllUserInfo extends Component {
 		});
 	}
 
+	//activates/shows user tab
+	AttendanceTabClick(event) {
+		this.setState({ 
+			AttendanceTab: 'active',
+			CareerTab:'inactive',
+			HomeworkTab: 'inactive'
+		});
+	}
+	//activates/shows section tab
+	CareerTabClick(event) {
+		this.setState({ 
+			AttendanceTab: 'inactive',
+			CareerTab:'active',
+			HomeworkTab: 'inactive'
+		});
+	}
+
+	HomeworkTabClick(event) {
+		this.setState({ 
+			AttendanceTab: 'inactive',
+			CareerTab:'inactive',
+			HomeworkTab: 'active'
+		});
+	}
 
 	render() {
-
+		console.log(this.props.params.id)
 		return (
 			
 			<div>
 				<CreateUserForm
 					sectionList = {this.state.sectionList}
 					UserFormType = 'update'
+					userId= {this.props.params.id}
 				/>
+				<ul className="nav nav-pills">
+					<li onClick={this.AttendanceTabClick} className={this.state.AttendanceTab}>
+						<a data-toggle="pill" href={"#/admin/user/" + this.props.params.id}>Attendance</a>
+					</li>
+					<li onClick={this.CareerTabClick} className={this.state.CareerTab}>
+						<a data-toggle="pill" href={"#/admin/user/" + this.props.params.id}>Career</a>
+					</li>
+					<li onClick={this.HomeworkTabClick} className={this.state.HomeworkTab}>
+						<a data-toggle="pill" href={"#/admin/user/" + this.props.params.id}>Homework</a>
+					</li>
+				</ul>
+
+				<div className="tab-content">
+					
+				</div>
 			</div>
 		);
 	}
